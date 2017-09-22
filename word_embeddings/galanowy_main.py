26c1a34451a996b32c8c1889bfab6c0825fbeaef
wojciech-galan <wojciech.galan@uj.edu.pl>
2017-09-22 17:40:29 +0200

    corrected paths

--- word_embeddings/galanowy_main.py
+++ word_embeddings/galanowy_main.py
@@ -3,9 +3,9 @@
 
 import os
 from src.disorder_parser import *
-# from keras.models import Sequential
-# from keras.layers import Dense
-# from keras.layers import LSTM
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
 
 
 # na razie na spokojnie bez maskowania
@@ -22,10 +22,12 @@ def get_DM_data(path):
     return representations , disorders
 
 def network(X_train, Y_train, X_test, Y_test):
+    # first try
     pass
-
+    # model = Sequential()
+    # print(len(X_train))
 
 if __name__ == '__main__':
-    X_train, Y_train = get_DM_data(open(os.path.join('..', "disorder", 'trainDM')).read())
-    X_test, Y_test = get_DM_data(open(os.path.join('..', "disorder", 'testDM')).read())
+    X_train, Y_train = get_DM_data(open(os.path.join("disorder", 'trainDM')).read())
+    X_test, Y_test = get_DM_data(open(os.path.join("disorder", 'testDM')).read())
     network(X_train, Y_train, X_test, Y_test)
